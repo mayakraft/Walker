a6006bf006ecdd7c328f5507ec7547b32b034a6c
robbykraft <robbykraft@gmail.com>
2013-11-01 11:48:09 -0700

    disable brightness fade

--- Walker/ViewController.m
+++ Walker/ViewController.m
@@ -22,8 +22,6 @@
     SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
-    [[UIScreen mainScreen] setBrightness:0.5];
-    
     // Present the scene.
     [skView presentScene:scene];
 }
