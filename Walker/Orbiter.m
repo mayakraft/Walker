9b67b098c335818675944a6b857b9ed188f2afb7
robbykraft <robbykraft@gmail.com>
2013-11-26 23:56:23 -0800

    orbit fits screen

--- Walker/Orbiter.m
+++ Walker/Orbiter.m
@@ -15,7 +15,7 @@
     float coords[2];  // [0]angle [1]distance
     float velocity;
     float acceleration;
-    
+    float radius;  // radius of orbit
     CGPoint center;
 }
 
@@ -29,6 +29,10 @@
         ta = arc4random()%100/100.0;
         td = ta+arc4random()%1000/100.0;
         STEP = .02;
+        if([[UIScreen mainScreen] bounds].size.width < [[UIScreen mainScreen] bounds].size.height)
+            radius = [[UIScreen mainScreen] bounds].size.width*.4;
+        else
+            radius = [[UIScreen mainScreen] bounds].size.height*.4;
         center = CGPointMake([[UIScreen mainScreen] bounds].size.width*.5, [[UIScreen mainScreen] bounds].size.height*.5);
     }
     return self;
@@ -38,7 +42,7 @@
     acceleration = .01*noise1(ta);
     velocity += acceleration;
     coords[0] += velocity;
-    coords[1] = 100+50*noise1(td);
+    coords[1] = radius+radius*.1*noise1(td);
     self.position = CGPointMake(center.x + coords[1]*sinf(coords[0]),
                                 center.y + coords[1]*cosf(coords[0]));
     ta+=STEP;
